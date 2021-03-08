bfe58ce3f0aaaa087f546bfd6eb20718fa8760e6
Pranav <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2021-03-08 15:24:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#input fed thru command line
+f = open("out.txt", "w")
+D,I,S,V,F =  [int(x) for x in input().split()]
+edges = []
+endpoints = []
+for i in range(S):
+    x,y,z,r = input().split()
+    edges.append([int(y),z])
+    endpoints.append(int(y))
+endpoints = list(set(endpoints))
+k = []
+for i in range(V):
+    l = list(input().split())
+    l = l[1:]
+    k.append(l)
+f.write(str(len(endpoints)))
+f.write("\n")
+for i in range(len(endpoints)):
+    f.write(str(endpoints[i])+"\n")
+    c = 0
+    for j in edges:
+        if(j[0] == endpoints[i]):
+            c=c+ 1
+    f.write(str(c)+"\n")
+    for j in edges:
+        if(j[0] == endpoints[i]):
+            f.write(j[1]+" "+"1"+"\n")
